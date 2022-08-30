56ecfda04b19b712c4d322fe14b216c9a697396e
Jacob Neitzell <jake.neitzell@hotmail.com>
2022-08-29 21:28:47 -0600

    Updated

--- app.js
+++ app.js
@@ -1,24 +1,57 @@
-let healthCount = 0
+let healthCount1 = 0
+let healthCount2 = 0
 
-
-
-function addHealth() {
-  healthCount++
+function addHealth1() {
+  healthCount1++
   draw()
 }
 
+function remHealth1() {
+  healthCount1--
+  drawfrom()
+}
 function draw() {
   // @ts-ignore
-  document.getElementById('healthCount').innerHTML = ` 
+  document.getElementById('healthCount1').innerHTML = ` 
       <div>
-      ${healthCount}
+      ${healthCount1}
       </div>`
 }
 
+function drawfrom() {
+  // @ts-ignore
+  document.getElementById('healthCount1').innerHTML = ` 
+      <div>
+      ${healthCount1}
+      </div>`
+}
+
+function addHealth2() {
+  healthCount2++
+  draw2()
+}
+
+function remHealth2() {
+  healthCount2--
+  drawfrom2()
+}
 
 
+function draw2() {
+  // @ts-ignore
+  document.getElementById('healthCount2').innerHTML = ` 
+      <div>
+      ${healthCount2}
+      </div>`
+}
 
-
+function drawfrom2() {
+  // @ts-ignore
+  document.getElementById('healthCount2').innerHTML = ` 
+      <div>
+      ${healthCount2}
+      </div>`
+}
 
 
 
